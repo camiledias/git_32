a1b62d7351faeba08c29d5beb4e76dcac5d747b2
camiledias <camilesdsilva@gmail.com>
2024-09-17 09:46:36 -0300

    Adicionado o link e botao

---
+++ pagina1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="pagina2.php">
+    <button type="submit">
+        Ir para a página 2
+ </button>
+</a>
+
+</body>
+</html>
